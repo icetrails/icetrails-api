b86f58c463d27be6523735fb7d911a488e3ccfb8
David Paschich <dpassage@balveda.com>
2014-01-04 11:19:12 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+IcetrailsApi::Application.config.secret_key_base = '587de271ac44473b31f4913137e30e71d91c78b3a632e42228b6b3b6695470ca2bdf3db9a3f0d357b0a09d2e87135368c4acb26c780e180192b64157ce6548ad'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+IcetrailsApi::Application.config.session_store :cookie_store, key: '_icetrails-api_session'
