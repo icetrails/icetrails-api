d7bbbe4087907c7f25dda5dd9025145c8cd0cc5c
David Paschich <dpassage@balveda.com>
2014-01-04 22:58:43 -0800

    events know how to alert themselves.

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,12 @@
 class Event < ActiveRecord::Base
   belongs_to :user
   has_many :locations
+
+  def send_alert
+    path = Rails.application.routes.url_helpers.event_path(self)
+    url = "icetrails://api.icetrails.com#{path}.json"
+    user.people_to_alert.each do |person|
+      SendSMS.send_message("#{user.name} may be in trouble! Click for more info: #{url}", person.phone)
+    end
+  end
 end
