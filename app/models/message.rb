743cc49bf3fa5a9d17bf213494e445adc13964f2
David Paschich <dpassage@balveda.com>
2014-01-05 01:05:45 -0800

    Process inbound messages, looking for responses to pings

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,16 @@
 class Message < ActiveRecord::Base
+  scope :unprocessed, -> { where(processed: false) }
+
+  def self.process_messages
+    Message.unprocessed.select { |m| m.text.downcase.start_with?("y") }.each do |message|
+      from = message.from
+      user = User.where(phone: from).first
+      user.events.each do |event|
+        event.ok_at = DateTime.now
+        event.save
+      end
+      message.processed = true
+      message.save
+    end
+  end
 end
