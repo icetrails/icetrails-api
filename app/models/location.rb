66e5d466cef115255902e58000e804f0fdcedd99
David Paschich <dpassage@balveda.com>
2014-01-04 16:57:15 -0800

    Create location entries for users from an event

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,3 +1,13 @@
 class Location < ActiveRecord::Base
   belongs_to :event
+
+  def self.create_latest_for_event(event)
+    token = event.user.token
+    headers = { Authorization: "Bearer #{token}", Accept: 'application/json' }
+    response = RestClient.get 'https://api.att.com/2/devices/location?requestedAccuracy=10000', headers
+    if response.code == 200
+      value = JSON.parse(response.body)
+      Location.create!(event: event, lat: value["latitude"], long: value["longitude"], time: DateTime.rfc3339(value["timestamp"]))
+    end
+  end
 end
